3c6c19cabda977783778673d7c19e7aaeecb0c5e
Gildas Lormeau <gildas.lormeau@gmail.com>
2016-09-17 03:33:13 +0200

    added IIFEs

--- scripts/bgpage/index.js
+++ scripts/bgpage/index.js
@@ -1,18 +1,23 @@
-/* global chrome, app */
+/* global chrome, window, app */
 
-const netfilters = app.misc.netfilters(chrome.webRequest);
-const configuration = app.configuration(chrome.storage);
-const welcome = app.welcome(chrome.runtime);
-const tabs = app.tabs(chrome.tabs, chrome.runtime);
-const ui = app.ui(chrome.browserAction, configuration, tabs);
-const network = app.network(configuration, netfilters);
-const business = app.business(configuration, tabs, network);
+(() => {
 
-/* exported init */
-var init = business.init;
+	const [welcome, ui, business] = (() => {
+		const netfilters = app.misc.netfilters(chrome.webRequest);
+		const configuration = app.configuration(chrome.storage);
+		const welcome = app.welcome(chrome.runtime);
+		const tabs = app.tabs(chrome.tabs, chrome.runtime);
+		const ui = app.ui(chrome.browserAction, configuration, tabs);
+		const network = app.network(configuration, netfilters);
+		const business = app.business(configuration, tabs, network);
+		return [welcome, ui, business];
+	})();
 
-welcome.onInstalled(ui.displayHelp);
-business.init();
-business.getConfiguration().then(ui.refreshBadge);
-business.onUpdate(ui.refreshIcon);
-ui.onButtonClick(() => business.toggleEnabled().then(ui.refreshBadge));
+	window.init = business.init;
+	welcome.onInstalled(ui.displayHelp);
+	business.init();
+	business.getConfiguration().then(ui.refreshBadge);
+	business.onUpdate(ui.refreshIcon);
+	ui.onButtonClick(() => business.toggleEnabled().then(ui.refreshBadge));
+
+})();
